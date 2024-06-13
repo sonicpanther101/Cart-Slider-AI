8f2fcdbb28ae21a48378de1b5f1833bfd93c11fc
sonicpanther101 <sonicpanther101@gmailcom>
2024-06-14 10:32:53 +1200

    work on parallel sorting

--- neural_network_parallel.py
+++ neural_network_parallel.py
@@ -1,2 +1,32 @@
 import physics_parallel as physics
+import cupy, copy
 
+def removeNodes(nodeIDs, nodeIDsToRemove, nodeParents):
+
+    filteredNodeIDs = list(set(nodeIDs) - set(nodeIDsToRemove))
+
+    nodeParents = nodeParents.reshape(-1, 1)
+    mask = ~cupy.isin(nodeParents.reshape(-1, 1), nodeIDsToRemove)
+    lenMask = mask.sum(axis=0)
+    filteredElements = nodeParents[mask.flatten()]
+    filteredNodeParents = filteredElements.reshape(lenMask.shape).tolist()
+
+    return filteredNodeIDs, filteredNodeParents
+
+def sortNodes(nodeIDs, nodeParents):
+
+    tempNodeIDs = nodeIDs[:]
+    tempNodeParents = copy.deepcopy(nodeParents)
+
+    sortedNodeIDs = cupy.array([])
+
+    while len(tempNodeIDs) > 0:
+
+        nodeIDsToProcess = cupy.array([])
+        nodeIDsToRemove = cupy.array([])
+
+        mask = cupy.shape(nodeParents)[1] == 0
+
+        sortedNodeIDs.extend(nodeIDs[mask])
+
+        tempNodeIDs, tempNodeParents = removeNodes(tempNodeIDs, nodeIDs[mask], nodeParents)
