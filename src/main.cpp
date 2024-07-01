d49e55a62a720a67bedef2ff745e2e719c0497c7
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-07-01 05:12:03 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,46 @@
-#include "neural_network.h"
-#include "physics.h"
+#include "neural_network.hpp"
+#include "physics.hpp"
 #include <iostream>
 #include <chrono>
+#include <vector>
 using namespace std;
 
-long double previousTime = 0;
-long double startTime
-    = chrono::duration_cast<chrono::duration<long double>>(
-        chrono::system_clock::now().time_since_epoch()).count();
-long double deltaTime = 0;
+auto previousTime = high_resolution_clock::now();
+auto currentTime = high_resolution_clock::now();
+long double deltaTime = 1 / 100;
+
+vector<Agent> agents[1000];
+int subSteps = 1;
+int frames = 0;
+
 size_t threadsToUse = 1;
-int step = static_cast<int>(generation.size()/threadsToUse);
+int step = (int)(agents[0].generation.size() / threadsToUse);
 
 int main() {
 
+    while (frames <= 1000) {
+        //++frames;
 
-    return 0;
-}
+        /*currentTime = high_resolution_clock::now();
+        deltaTime = duration_cast<duration<long double>>(currentTime - previousTime).count();
+        previousTime = currentTime;
+
+        if (deltaTime >= 0.01) {
+            deltaTime = 0;
+        }*/
+
+        for (size_t i = 0; i < agents.size(); i++) {
+
+            agents[i].environment.update(deltaTime);
+
+            agents[i].brain[0].value = agents[i].environment.balls[0].position[0];
+            agents[i].brain[1].value = agents[i].environment.balls[0].position[0];
+            agents[i].brain[2].value = agents[i].environment.balls[0].position[0];
+            agents[i].brain[3].value = agents[i].environment.balls[0].position[0];
+        }
+
+
+        environment.cartVelocity = 10;
+
+        return 0;
+    }
